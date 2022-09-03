c8da6e30ad3f713d5adf727b633450902976a5f3
taticorredor <jennifert.corredor@gmail.com>
2022-09-02 22:02:34 -0500

    Contenido de la clase movimiento dinero

--- src/main/java/com/example/ProyectoBreadCiclo3/MovimientoDinero.java
+++ src/main/java/com/example/ProyectoBreadCiclo3/MovimientoDinero.java
@@ -1,4 +1,43 @@
 package com.example.ProyectoBreadCiclo3;
 
-public class MovimientoDinero {
+public class movimientoDinero {
+
+    private int montoMovimiento;
+
+    private String conceptoMovimiento;
+
+    private Empleado empleado;
+
+    //CONSTRUCTOR
+
+    public movimientoDinero(int montoMovimiento, String conceptoMovimiento, Empleado empleado) {
+        this.montoMovimiento = montoMovimiento;
+        this.conceptoMovimiento = conceptoMovimiento;
+        this.empleado = empleado;
+    }
+
+    //GETTER
+    public int getMontoMovimiento() {
+        return montoMovimiento;
+    }
+
+    public void setMontoMovimiento(int montoMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+    }
+
+    public String getConceptoMovimiento() {
+        return conceptoMovimiento;
+    }
+
+    public void setConceptoMovimiento(String conceptoMovimiento) {
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
 }
