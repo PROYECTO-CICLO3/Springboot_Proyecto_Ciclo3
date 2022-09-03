5ea44a387aeaae79aa9fd07c4718f8be196e4c63
CcgedarK <ccge.555@gmail.com>
2022-09-02 21:05:45 -0500

    Contenido de la clase empresa

--- src/main/java/com/example/ProyectoBreadCiclo3/Empresa.java
+++ src/main/java/com/example/ProyectoBreadCiclo3/Empresa.java
@@ -2,4 +2,54 @@ package com.example.ProyectoBreadCiclo3;
 
 public class Empresa {
 
+    private String nombreEmpresa;
+    private String direccionEmpresa;
+    private int telefonoEmpresa;
+    //Posible primaryKey
+    private int NITEmpresa;
+
+
+    //CONSTRUCTOR
+
+    public Empresa(String nombreEmpresa, String direccionEmpresa, int telefonoEmpresa, int NITEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+        this.direccionEmpresa = direccionEmpresa;
+        this.telefonoEmpresa = telefonoEmpresa;
+        this.NITEmpresa = NITEmpresa;
+    }
+
+
+    //GETTER AND SETTERS
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getDireccionEmpresa() {
+        return direccionEmpresa;
+    }
+
+    public void setDireccionEmpresa(String direccionEmpresa) {
+        this.direccionEmpresa = direccionEmpresa;
+    }
+
+    public int getTelefono() {
+        return telefonoEmpresa;
+    }
+
+    public void setTelefono(int telefonoEmpresa) {
+        this.telefonoEmpresa = telefonoEmpresa;
+    }
+
+    public int getNITEmpresa() {
+        return NITEmpresa;
+    }
+
+    public void setNITEmpresa(int NITEmpresa) {
+        this.NITEmpresa = NITEmpresa;
+    }
 }
