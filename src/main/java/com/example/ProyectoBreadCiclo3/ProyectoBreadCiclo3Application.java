b58935767dbf6d18220edc268cec46fc07bb74b2
CcgedarK <ccge.555@gmail.com>
2022-09-02 20:31:45 -0500

    Creacion de poryecto en Springboot

---
+++ src/main/java/com/example/ProyectoBreadCiclo3/ProyectoBreadCiclo3Application.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoBreadCiclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBreadCiclo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBreadCiclo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProyectoBreadCiclo3/ProyectoBreadCiclo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoBreadCiclo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBreadCiclo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
